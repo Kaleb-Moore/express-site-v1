199f0e87772c224a31e6f36ee7c22debe12d58ac
kaleb-moore <k4l3bmoore@gmail.com>
2021-07-17 12:01:15 -0600

    Clean up
    Moves image files to public folder and starts displaying them on the main screen

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const express = require('express');
+const router = express.Router();
 const path = require('path');
 const { projects } = require('./data.json');
 
-const indexRoute = require('./views/index.pug');
-// const aboutRoute = require('./views/about');
-
 const app = express();
 
- app.set('view engine', 'pug');
- app.use('/static', express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'))
 
- app.use('/', indexRoute)
-//  app.get('/', (req, res, next) => {
-//      res.locals = projects;
-//      res.render('index', indexRoute);
-//  });
-
- app.get('/about', (req, res, next) => {
-     res.render('about')
+ app.get('/', (req, res, next) => {
+     res.locals = projects;
+     res.render('index', { projects });
  });
 
- app.get('/projects/:id', (req, res, next) => {
-    const projectId = req.params.id;
-    const project = projects.find( ({ id }) => id === +projectId );
+app.get('/about', (req, res, next) => {
+    res.render('about')
+});
 
-    res.render('project', { project });
- });
+app.get('/projects/:id', (req, res, next) => {
+const projectId = req.params.id;
+const project = projects.find( ({ id }) => id === +projectId );
 
- app.listen(3000, "Port Listening on localhost:3000");
+res.render('project', { project });
+});
+
+app.listen(3000, () => {
+    console.log("Port Listening on localhost:3000");
+});
